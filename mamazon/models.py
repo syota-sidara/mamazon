cea19bb5039eb94902ea668262c9a6d8112716a4
syota-sidara <ss045.official@gmail.com>
2020-05-26 16:58:46 +0900

    lesson04

--- mamazon/models.py
+++ mamazon/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 # models定義後はmigrationを忘れずに
+
+
 class Product(models.Model):
-  # CharFieldを使う際はmax_lengthは必ず必要
-  name = models.CharField(max_length=150)
-  price = models.IntegerField()
-  description = models.TextField()
-  thumbnail = models.ImageField(upload_to='thumbnails/')
+    # CharFieldを使う際はmax_lengthは必ず必要
+    name = models.CharField(max_length=150)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+
+    def __str__(self):
+        return self.name
